cdb72fd039b1a122d6626ece003629fb284e54be
ronniel <ronniel@automationlab.com.au>
2018-06-06 15:44:44 +0800

    Checking CI

--- test.php
+++ test.php
@@ -1,11 +1,7 @@
 <?php 
-echo 'sample test CI';
-echo '2 test success';
-echo 'sample test CI';
-echo '3 test success';
-
-echo 'sample test CI';
-echo '4 test success';
-echo '4 test success';
+echo sample();
+function sample(){
+	return 'sample';
+}
 
 ?>
